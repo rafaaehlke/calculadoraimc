60d5c53b38207bb9d53d4862925e39d5cca87d98
Rafael Ehlke <heeyrafaa@gmail.com>
2024-05-08 22:25:28 -0300

    add alert error

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import { Modal } from "./modal.js";
+import { AlertError } from "./alert-error.js"
 
 const form = document.querySelector("form");
 const inputPeso = document.querySelector("#peso");
@@ -15,12 +16,25 @@ form.onsubmit = (event) => {
   const peso = inputPeso.value;
   const altura = inputAltura.value;
 
+  const showAlertError = notANumber(peso) || notANumber(altura)
+
+  if (showAlertError) {
+    AlertError.open()
+    return
+  }
+
+  AlertError.close()
+
   const result = imcCalc(peso, altura);
   const message = `Seu IMC é de ${result}`;
 
   Modal.message.innerText = message;
   Modal.open();
 };
+
+function notANumber(value)  {
+  return isNaN(value) || value === ""
+}
 
 function imcCalc(peso, altura) {
   return (peso / (altura / 100) ** 2).toFixed(2);
